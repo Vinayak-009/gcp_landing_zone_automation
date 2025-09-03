a98d12377adac166a0ac1658cc3e9b47b6b48e7f
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2025-09-03 11:27:40 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,9 @@
 output "created_folders" {
-  description = "A map of created folders, keyed by their full path, with their IDs as values."
-  value = {
-    for key, folder in google_folder.main :
-    key => folder.id
-  }
+  description = "A map of all created folders and their IDs."
+  value       = { for key, folder in google_folder.main : key => folder.id }
 }
 
 output "created_projects" {
-  description = "A map of created projects, keyed by their project ID, with their numeric project numbers as values."
-  value = {
-    for key, project in google_project.main :
-    key => project.number
-  }
+  description = "A map of all created projects and their numeric project numbers."
+  value       = { for key, project in google_project.main : key => project.number }
 }
