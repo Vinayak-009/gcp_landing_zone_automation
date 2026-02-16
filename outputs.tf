22ebfa44f4e24b8d2ec06fefcd322e9c9b1b37bf
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2026-02-17 04:27:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,15 @@
 output "created_folders" {
-  description = "A map of all created folders and their IDs (numeric)."
-  value       = local.all_folder_ids
+  description = "A map of all created folders and their resource names (folders/id)."
+  # Changed from local.all_folder_ids to local.all_folders
+  value       = local.all_folders
 }
 
 output "created_projects" {
   description = "A map of all created projects and their numeric project numbers."
   value       = { for key, project in google_project.main : key => project.number }
+}
+
+output "vm_instances" {
+  description = "Details of the created VM instances."
+  value       = module.compute_instances
 }
